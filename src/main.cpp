752fc49a2d1238a64a8dbc3ccf43d0c09cb3c658
Alessio Sclocco <alessio@sclocco.eu>
2013-11-14 15:55:02 +0100

    Added output handling and memory allocation.

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::fixed;
 #include <string>
 using std::string;
 #include <exception>
@@ -31,9 +32,13 @@ using std::exception;
 using std::ofstream;
 #include <vector>
 using std::vector;
+#include <iomanip>
+using std::setprecision;
 
 #include <ArgumentList.hpp>
 using isa::utils::ArgumentList;
+#include <utils.hpp>
+using isa::utils::giga;
 #include <Observation.hpp>
 using AstroData::Observation;
 #include <ReadData.hpp>
@@ -41,12 +46,16 @@ using AstroData::readSIGPROC;
 using AstroData::readLOFAR;
 #include <Dedispersion.hpp>
 using PulsarSearch::Dedispersion;
+#include <Shifts.hpp>
+using PulsarSearch::getShifts;
 #include <Folding.hpp>
 using PulsarSearch::Folding;
 #include <SNR.hpp>
 using PulsarSearch::SNR;
 #include <CLData.hpp>
 using isa::OpenCL::CLData;
+#include <Exceptions.hpp>
+using isa::Exceptions::OpenCLError;
 
 
 int main(int argc, char * argv[]) {
@@ -97,12 +106,108 @@ int main(int argc, char * argv[]) {
 	// Load observation data
 	vector< CLData< dataType > * > * input = new vector< CLData< dataType > * >(1);
 	if ( dataLOFAR ) {
-		readLOFAR(headerFile, dataFile, observation, *input);
+		readLOFAR(headerFile, dataFile, obs, *input);
 	} else if ( SIGPROC ) {
 		// TODO: implement SIGPROC pipeline
 	}
 
+	// Initialize OpenCL
+	cl::Context *clContext = new cl::Context();
+	vector< cl::Platform > *clPlatforms = new vector< cl::Platform >();
+	vector< cl::Device > *clDevices = new vector< cl::Device >();
+	vector< vector< cl::CommandQueue > > *clQueues = new vector< vector < cl::CommandQueue > >();
+	
+	try {
+		initializeOpenCL(clPlatformID, 1, clPlatforms, clContext, clDevices, clQueues);
+	} catch ( OpenCLError err ) {
+		cerr << err.what() << endl;
+		return 1;
+	}
+
+	// Memory allocation
+	unsigned int nrSamplesPerChannel = 0;
+	unsigned int secondsToBuffer = 0;
+	CLData< unsigned int > * shifts = getShifts(obs);
+	CLData< dataType > dispersedData("DispersedData", true);
+	CLData< dataType > dedispersedData("DedispersedData", true);
+	CLData< dataType > transposedData("TransposedData", true);
+	CLData< dataType > foldedData("FoldedData", true);
+	CLData< unsigned int > counterData("CounterData", true);
+	CLData< dataType > snrTable("SNRTable", true);
+
+	if ( ((obs.getNrSamplesPerSecond() + (*shifts)[((obs.getNrDMs() - 1) * obs.getNrPaddedChannels())]) % obs.getPadding()) != 0 ) {
+		nrSamplesPerChannel = (obs.getNrSamplesPerSecond() + (*shifts)[((obs.getNrDMs() - 1) * obs.getNrPaddedChannels())]) + (obs.getPadding() - ((obs.getNrSamplesPerSecond() + (*shifts)[((obs.getNrDMs() - 1) * obs.getNrPaddedChannels())]) % obs.getPadding()));
+	} else {
+		nrSamplesPerChannel = (obs.getNrSamplesPerSecond() + (*shifts)[((obs.getNrDMs() - 1) * obs.getNrPaddedChannels())]);
+	}
+	secondsToBuffer = static_cast< unsigned int >(ceil(static_cast< float >(nrSamplesPerChannel) / obs.getNrSamplesPerPaddedSecond()));
+
+	dispersedData.allocateHostData(secondsToBuffer * observation.getNrChannels() * observation.getNrSamplesPerPaddedSecond());
+	snrTable.allocateHostData(obs.getNrPeriods() * obs.getNrPaddedDMs());
+
+	try {
+		shifts->setCLContext(clContext);
+		shifts->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		shifts->allocateDeviceData();
+		shifts->copyHostToDevice(true);
+		dispersedData.setCLContext(clContext);
+		dispersedData.setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		dispersedData.allocateDeviceData();
+		dedispersedData.setCLContext(clContext);
+		dedispersedData.setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		dedispersedData.allocateDeviceData(obs.getNrDMs() * obs.getNrSamplesPerPaddedSecond());
+		transposedData.setCLContext(clContext);
+		transposedData.setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		transposedData.allocateDeviceData(obs.getNrSamplesPerSecond() * obs.getNrPaddedDMs());
+		foldedData.setCLContext(clContext);
+		foldedData.setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		foldedData.allocateDeviceData(obs.getNrBins() * obs.getNrPeriods() * obs.getNrPaddedDMs());
+		foldedData.blankHostData();
+		foldedData.allocateDeviceData();
+		foldedData.copyHostToDevice();
+		foldedData.deleteHostData();
+		counterData.setCLContext(clContext);
+		counterData.setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
+		counterData.allocateDeviceData(obs.getNrBins() * obs.getNrPeriods() * obs.getNrPaddedDMs());
+		counterData.blankHostData();
+		counterData.allocateDeviceData();
+		counterData.copyHostToDevice();
+		counterData.deleteHostData();
+	} catch ( OpenCLError err ) {
+		cerr << err.what() << endl;
+		return 1;
+	}
+
+	if ( DEBUG ) {
+		double hostMemory = 0.0;
+		double deviceMemory = 0.0;
+		hostMemory += shifts.getHostDataSize() + dispersedData.getHostDataSize() + snrTable.getHostDataSize();
+		deviceMemory += shifts.getDeviceDataSize() + dispersedData.getDeviceDataSize() + dedispersedData.getDeviceDataSize() + transposedData.getDeviceDataSize() + foldedData.getDeviceDataSize() + counterData.getDeviceDataSize() + snrTable.getDeviceDataSize();
+
+		cout << "Allocated host memory: " << fixed << setprecision(3) << giga(hostMemory) << endl;
+		cout << "Allocated device memory: " << fixed << setprecision(3) << giga(deviceMemory) << endl;
+	}
+
+	// Search loop
+	for ( unsigned int second = 0; second < obs.getNrSeconds(); second++ ) {
+
+	}
+
 	// Store output
+	try {
+		snrTable.copyDeviceToHost();
+	} catch ( OpenCLError err ) {
+		cerr << err.what() << endl;
+		return 1;
+	}
+	ofstream output;
+	output.open(outputFile);
+	for ( unsigned int period = 0; period < obs.getNrPeriods(); period++ ) {
+		for ( unsigned int dm = 0; dm < obs.getNrDMs(); dm++ ) {
+			output << period << " " << dm << " " << fixed << setprecision(3) << snrTable[(period * obs.getNrPaddedDMs()) + dm] << endl;
+		}
+	}
+	output.close();
 
 	return 0;
 }
