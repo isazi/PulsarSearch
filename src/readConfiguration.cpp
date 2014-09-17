299d28d3e31dda73fe68d08d8d2f5a81ae37ce38
Alessio Sclocco <alessio@sclocco.eu>
2014-09-17 14:42:29 +0200

    Style.

--- src/readConfiguration.cpp
+++ src/readConfiguration.cpp
@@ -13,108 +13,102 @@
 // limitations under the License.
 
 #include <string>
-using std::getline;
 #include <fstream>
-using std::ifstream;
 #include <map>
-using std::make_pair;
 #include <vector>
-using std::vector;
 #include <cctype>
-using std::isalpha;
 
 #include <readConfiguration.hpp>
 #include <utils.hpp>
-using isa::utils::castToType;
 
 
-void readPadding(map< string, unsigned int > & padding, const string paddingFilename) {
-	string temp;
-	ifstream paddingFile(paddingFilename);
+void readPadding(map< std::string, unsigned int > & padding, const std::string & paddingFilename) {
+	std::string temp;
+	std::ifstream paddingFile(paddingFilename);
 
 	while ( ! paddingFile.eof() ) {
 		unsigned int middle = 0;
 
-		getline(paddingFile, temp);
-		if ( ! isalpha(temp[0]) ) {
+		std::getline(paddingFile, temp);
+		if ( ! std::isalpha(temp[0]) ) {
 			continue;
 		}
 		middle = temp.find(" ");
-		padding.insert(make_pair(temp.substr(0, middle), castToType< string, unsigned int >(temp.substr(middle + 1))));
+		padding.insert(std::make_pair(temp.substr(0, middle), isa::utils::castToType< std::string, unsigned int >(temp.substr(middle + 1))));
 	}
 }
 
-void readVectorWidth(map< string, unsigned int > & vectorWidth, const string vectorFilename) {
-	string temp;
-	ifstream vectorFile(vectorFilename);
+void readVectorWidth(map< std::string, unsigned int > & vectorWidth, const std::string & vectorFilename) {
+	std::string temp;
+	std::ifstream vectorFile(vectorFilename);
 
 	while ( ! vectorFile.eof() ) {
 		unsigned int middle = 0;
 
-		getline(vectorFile, temp);
-		if ( ! isalpha(temp[0]) ) {
+		std::getline(vectorFile, temp);
+		if ( ! std::isalpha(temp[0]) ) {
 			continue;
 		}
 		middle = temp.find(" ");
-		vectorWidth.insert(make_pair(temp.substr(0, middle), castToType< string, unsigned int >(temp.substr(middle + 1))));
+		vectorWidth.insert(std::make_pair(temp.substr(0, middle), isa::utils::castToType< std::string, unsigned int >(temp.substr(middle + 1))));
 	}
 }
 
-void readDedispersion(map< string, map< unsigned int, vector< unsigned int > > > & dedispersionParameters, const string dedispersionFilename) {
-	string temp;
-	ifstream dedispersionFile(dedispersionFilename);
+void readDedispersion(map< std::string, map< unsigned int, std::vector< unsigned int > > > & dedispersionParameters, const std::string  & dedispersionFilename) {
+	std::string temp;
+	std::ifstream dedispersionFile(dedispersionFilename);
 
 	while ( ! dedispersionFile.eof() ) {
 		unsigned int splitPoint = 0;
 
-		getline(dedispersionFile, temp);
-		if ( ! isalpha(temp[0]) ) {
+		std::getline(dedispersionFile, temp);
+		if ( ! std::isalpha(temp[0]) ) {
 			continue;
 		}
-		string deviceName;
+		std::string deviceName;
 		unsigned int nrDMs = 0;
-		vector< unsigned int > parameters(4);
+		std::vector< unsigned int > parameters(4);
 
 		splitPoint = temp.find(" ");
 		deviceName = temp.substr(0, splitPoint);
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		nrDMs = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		nrDMs = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[0] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[0] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[1] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[1] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[2] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[2] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
-		parameters[3] = castToType< string, unsigned int >(temp);
+		parameters[3] = isa::utils::castToType< std::string, unsigned int >(temp);
 
 		if ( dedispersionParameters.count(deviceName) == 0 ) {
-			map< unsigned int, vector< unsigned int > > container;
+			map< unsigned int, std::vector< unsigned int > > container;
 
-			container.insert(make_pair(nrDMs, parameters));
-			dedispersionParameters.insert(make_pair(deviceName, container));
+			container.insert(std::make_pair(nrDMs, parameters));
+			dedispersionParameters.insert(std::make_pair(deviceName, container));
 		} else {
-			dedispersionParameters[deviceName].insert(make_pair(nrDMs, parameters));
+			dedispersionParameters[deviceName].insert(std::make_pair(nrDMs, parameters));
 		}
 	}
 }
 
-void readTranspose(map< string, map< unsigned int, unsigned int > > & transposeParameters, const string transposeFilename) {
-	string temp;
-	ifstream transposeFile(transposeFilename);
+void readTranspose(map< std::string, map< unsigned int, unsigned int > > & transposeParameters, const std::string & transposeFilename) {
+	std::string temp;
+	std::ifstream transposeFile(transposeFilename);
 
 	while ( ! transposeFile.eof() ) {
 		unsigned int splitPoint = 0;
 
-		getline(transposeFile, temp);
-		if ( ! isalpha(temp[0]) ) {
+		std::getline(transposeFile, temp);
+		if ( ! std::isalpha(temp[0]) ) {
 			continue;
 		}
-		string deviceName;
+		std::string deviceName;
 		unsigned int nrDMs = 0;
 		unsigned int parameter = 0;
 
@@ -122,125 +116,125 @@ void readTranspose(map< string, map< unsigned int, unsigned int > > & transposeP
 		deviceName = temp.substr(0, splitPoint);
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		nrDMs = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		nrDMs = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
-		parameter = castToType< string, unsigned int >(temp);
+		parameter = isa::utils::castToType< std::string, unsigned int >(temp);
 
 		if ( transposeParameters.count(deviceName) == 0 ) {
 			map< unsigned int, unsigned int > container;
 
-			container.insert(make_pair(nrDMs, parameter));
-			transposeParameters.insert(make_pair(deviceName, container));
+			container.insert(std::make_pair(nrDMs, parameter));
+			transposeParameters.insert(std::make_pair(deviceName, container));
 		} else {
-			transposeParameters[deviceName].insert(make_pair(nrDMs, parameter));
+			transposeParameters[deviceName].insert(std::make_pair(nrDMs, parameter));
 		}
 	}
 }
 
-void readFolding(map< string, map< unsigned int, map< unsigned int, vector< unsigned int > > > > & foldingParameters, const string foldingFilename) {
-	string temp;
-	ifstream foldingFile(foldingFilename);
+void readFolding(map< std::string, map< unsigned int, map< unsigned int, std::vector< unsigned int > > > > & foldingParameters, const std::string & foldingFilename) {
+	std::string temp;
+	std::ifstream foldingFile(foldingFilename);
 
 	while ( ! foldingFile.eof() ) {
 		unsigned int splitPoint = 0;
 
-		getline(foldingFile, temp);
-		if ( ! isalpha(temp[0]) ) {
+		std::getline(foldingFile, temp);
+		if ( ! std::isalpha(temp[0]) ) {
 			continue;
 		}
-		string deviceName;
+		std::string deviceName;
 		unsigned int nrDMs = 0;
 		unsigned int nrPeriods = 0;
-		vector< unsigned int > parameters(6);
+		std::vector< unsigned int > parameters(6);
 
 		splitPoint = temp.find(" ");
 		deviceName = temp.substr(0, splitPoint);
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		nrDMs = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		nrDMs = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		nrPeriods = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		nrPeriods = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[0] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[0] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[1] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[1] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[2] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[2] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[3] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[3] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[4] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[4] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
-		parameters[5] = castToType< string, unsigned int >(temp);
+		parameters[5] = isa::utils::castToType< std::string, unsigned int >(temp);
 
 		if ( foldingParameters.count(deviceName) == 0 ) {
-			map< unsigned int, map< unsigned int, vector< unsigned int > > > externalContainer;
-			map< unsigned int, vector< unsigned int > > internalContainer;
+			map< unsigned int, map< unsigned int, std::vector< unsigned int > > > externalContainer;
+			map< unsigned int, std::vector< unsigned int > > internalContainer;
 
-			internalContainer.insert(make_pair(nrPeriods, parameters));
-			externalContainer.insert(make_pair(nrDMs, internalContainer));
-			foldingParameters.insert(make_pair(deviceName, externalContainer));
+			internalContainer.insert(std::make_pair(nrPeriods, parameters));
+			externalContainer.insert(std::make_pair(nrDMs, internalContainer));
+			foldingParameters.insert(std::make_pair(deviceName, externalContainer));
 		} else if ( foldingParameters[deviceName].count(nrDMs) == 0 ) {
-			map< unsigned int, vector< unsigned int > > internalContainer;
+			map< unsigned int, std::vector< unsigned int > > internalContainer;
 
-			internalContainer.insert(make_pair(nrPeriods, parameters));
-			foldingParameters[deviceName].insert(make_pair(nrDMs, internalContainer));
+			internalContainer.insert(std::make_pair(nrPeriods, parameters));
+			foldingParameters[deviceName].insert(std::make_pair(nrDMs, internalContainer));
 		} else {
-			foldingParameters[deviceName][nrDMs].insert(make_pair(nrPeriods, parameters));
+			foldingParameters[deviceName][nrDMs].insert(std::make_pair(nrPeriods, parameters));
 		}
 	}
 }
 
-void readSNR(map< string, map< unsigned int, map< unsigned int, vector< unsigned int > > > > & snrParameters, const string snrFilename) {
-	string temp;
-	ifstream snrFile(snrFilename);
+void readSNR(map< std::string, map< unsigned int, map< unsigned int, std::vector< unsigned int > > > > & snrParameters, const std::string & snrFilename) {
+	std::string temp;
+	std::ifstream snrFile(snrFilename);
 
 	while ( ! snrFile.eof() ) {
 		unsigned int splitPoint = 0;
 
-		getline(snrFile, temp);
-		if ( ! isalpha(temp[0]) ) {
+		std::getline(snrFile, temp);
+		if ( ! std::isalpha(temp[0]) ) {
 			continue;
 		}
-		string deviceName;
+		std::string deviceName;
 		unsigned int nrDMs = 0;
 		unsigned int nrPeriods = 0;
-		vector< unsigned int > parameters(2);
+		std::vector< unsigned int > parameters(2);
 
 		splitPoint = temp.find(" ");
 		deviceName = temp.substr(0, splitPoint);
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		nrDMs = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		nrDMs = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		nrPeriods = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		nrPeriods = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
 		splitPoint = temp.find(" ");
-		parameters[0] = castToType< string, unsigned int >(temp.substr(0, splitPoint));
+		parameters[0] = isa::utils::castToType< std::string, unsigned int >(temp.substr(0, splitPoint));
 		temp = temp.substr(splitPoint + 1);
-		parameters[1] = castToType< string, unsigned int >(temp);
+		parameters[1] = isa::utils::castToType< std::string, unsigned int >(temp);
 
 		if ( snrParameters.count(deviceName) == 0 ) {
-			map< unsigned int, map< unsigned int, vector< unsigned int > > > externalContainer;
-			map< unsigned int, vector< unsigned int > > internalContainer;
+			map< unsigned int, map< unsigned int, std::vector< unsigned int > > > externalContainer;
+			map< unsigned int, std::vector< unsigned int > > internalContainer;
 
-			internalContainer.insert(make_pair(nrPeriods, parameters));
-			externalContainer.insert(make_pair(nrDMs, internalContainer));
-			snrParameters.insert(make_pair(deviceName, externalContainer));
+			internalContainer.insert(std::make_pair(nrPeriods, parameters));
+			externalContainer.insert(std::make_pair(nrDMs, internalContainer));
+			snrParameters.insert(std::make_pair(deviceName, externalContainer));
 		} else if ( snrParameters[deviceName].count(nrDMs) == 0 ) {
-			map< unsigned int, vector< unsigned int > > internalContainer;
+			map< unsigned int, std::vector< unsigned int > > internalContainer;
 
-			internalContainer.insert(make_pair(nrPeriods, parameters));
-			snrParameters[deviceName].insert(make_pair(nrDMs, internalContainer));
+			internalContainer.insert(std::make_pair(nrPeriods, parameters));
+			snrParameters[deviceName].insert(std::make_pair(nrDMs, internalContainer));
 		} else {
-			snrParameters[deviceName][nrDMs].insert(make_pair(nrPeriods, parameters));
+			snrParameters[deviceName][nrDMs].insert(std::make_pair(nrPeriods, parameters));
 		}
 	}
 }
